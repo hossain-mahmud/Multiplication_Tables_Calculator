69f23d6ef86315c667e27794ab3d4f6dc535c745
Hossain Mahmud <hossainmahmud408@gmail.com>
2023-10-05 02:41:51 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,13 @@ let button = document.querySelector('#button');
 let result = document.querySelector('#result');
 
 button.addEventListener('click', function () {
-
     result.innerHTML = '';
-
-    for (let i = 1; i <= 10; i++) {
+        if (input_bar.value > 0) {
+            for (let i = 1; i <= 10; i++) {
         result.innerHTML += input_bar.value + '  X ' + i + ' = ' + input_bar.value * i + '</br>';
-
-        if (input_bar.value <= 0) {
-            result.innerHTML = "Please Input Right Value !!"
+            }
         }
-    }
+           else{
+             result.innerHTML = "Please Input Right Value !!"
+        }
 })
